94d7dde88fb81242c0a5f282f58840183d836243
Anthony <frostanton09@gmail.com>
2019-03-14 22:26:53 +0300

    Test

---
+++ fapi/src/main/java/com/netcracker/edu/fapi/controllers/SimpleController.java
@@ -0,0 +1,18 @@
+package com.netcracker.edu.fapi.controllers;
+
+import com.netcracker.edu.fapi.models.SimpleObject;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping(value = "/get-simple-text")
+    public SimpleObject getSimpleString(){
+        SimpleObject simpleObject = new SimpleObject();
+        simpleObject.setId("1");
+        simpleObject.setName("Alex");
+        return simpleObject;
+    }
+
+}
