2a39342d791d1b9d6ded0744eeb64debe2e83153
Anthony <frostanton09@gmail.com>
2019-04-08 22:42:46 +0300

    Add Price controller, repository, service

---
+++ backend/src/main/java/com/netcracker/edu/back/backend/service/PriceService.java
@@ -0,0 +1,9 @@
+package com.netcracker.edu.back.backend.service;
+
+import com.netcracker.edu.back.backend.entity.Price;
+
+import java.util.List;
+
+public interface PriceService {
+    List<Price> findAll();
+}
